6567086fe2b8c54f6f551e8ffac4e9962359f228
Mridul Verma <mridulv09@gmail.com>
2014-09-10 02:07:30 +0530

    Completed SideBar functionality

--- src/main/java/hello/SideBar.java
+++ src/main/java/hello/SideBar.java
@@ -7,9 +7,12 @@ import java.io.InputStream;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import opennlp.tools.postag.POSModel;
@@ -18,7 +21,6 @@ import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
 import opennlp.tools.util.InvalidFormatException;
-import ch.qos.logback.core.boolex.Matcher;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
@@ -47,6 +49,8 @@ public class SideBar {
     		Twitter twitter = new TwitterFactory().getInstance();
     		entity = entity.substring(1,entity.length());
             user = twitter.showUser(entity);
+            
+            id = user.getId();
             
             hashPresent = 0;
     	}
@@ -96,10 +100,12 @@ public class SideBar {
         Connection connection = (Connection) DriverManager.getConnection(conn.dbUrl, conn.username, conn.password);
         
         String query = "SELECT * FROM analysis_tweets_new "+init_query+" and lang LIKE 'en'";
+        System.out.println(query);
         Statement stmt = (Statement) connection.createStatement();
         ResultSet rs = stmt.executeQuery(query);
         
         Map<String, Double> hashMap = new HashMap<String, Double>();
+        Map<String, Double> hashMap2 = new HashMap<String, Double>();
         helper bvc =  new helper(hashMap);
         TreeMap<String,Double> sorted_map = new TreeMap<String,Double>(bvc);
         
@@ -114,10 +120,10 @@ public class SideBar {
             double rating = rs.getLong("rating");
 
             int keyword_value = 0;
-            text = text.toLowerCase().replaceAll("@\\p{L}+","").replaceAll("#\\p{L}+", "").replaceAll("[^'\\w\\s\\,]", "").replaceAll("http\\s*(\\w+)", "");
+            text = text.toLowerCase().replaceAll("@\\p{L}+","").replaceAll("#\\p{L}+", "").replaceAll("[^'\\w\\s\\,]", "").replaceAll("'\\p{L}+"," ").replaceAll("http\\s*(\\w+)", "");
             String tokens[] = tokenizer.tokenize(text);
             String arrText[] = tagger.tag(tokens);
-
+            
             for (String match : arrText) {
                 java.util.regex.Matcher matcher = Pattern.compile("N\\s*(\\w+)").matcher(match);
                 if (matcher.find()){
@@ -136,40 +142,114 @@ public class SideBar {
         }
         
         for(String key : hashMap.keySet()){
-     	   double value = hashMap.get(key);
-     	   
-            String queryNew = "SELECT * FROM keyword_new WHERE term LIKE '"+key+"' ";
+      	    double value = hashMap.get(key);
+      	    
+            String queryNew = "SELECT * FROM keywords_new WHERE term LIKE '"+key+"' ";
             Statement stmtNew = (Statement) connection.createStatement();
             ResultSet rsNew = stmtNew.executeQuery(queryNew);
             
             if (rsNew.absolute(1)){
          	   double num = rsNew.getDouble("value");
          	   value = value/num;
-         	   hashMap.put(key,value);
+         	   hashMap2.put(key,value);
             }
         }
         
-        sorted_map.putAll(hashMap);
-        
+        sorted_map.putAll(hashMap2);
         count = 0;
         
-        for(String key : hashMap.keySet()){
+        for(String key : sorted_map.keySet()){
         	JSONObject json = new JSONObject();
-        	double value = hashMap.get(key);
+        	double value = hashMap2.get(key);
         	json.put("key", key);
         	json.put("value", value);
         	mainjson.put(json);
         	
         	count++;
-        	if (count ==5)
+        	if (count == 5)
         		break;
         }
 		
 		return mainjson;
 	}
 	
-	public JSONArray getHashTagInformation(){
+	private JSONArray getInfo(String query) throws ClassNotFoundException, SQLException, JSONException{
 		JSONArray mainjson = new JSONArray();
+		
+		Class.forName(conn.dbClass);
+        Connection connection = (Connection) DriverManager.getConnection(conn.dbUrl, conn.username, conn.password);
+
+        Statement stmt = (Statement) connection.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        
+        Map<String, Double> hashMap = new HashMap<String, Double>();
+        Map<String, Double> hashMap2 = new HashMap<String, Double>();
+        helper bvc =  new helper(hashMap);
+        TreeMap<String,Double> sorted_map = new TreeMap<String,Double>(bvc);
+
+        int count = 0;
+
+        Stopwords stopwords = new Stopwords();
+
+        while (rs.next()) {
+        	String text = rs.getString("tweet");
+            double rating = rs.getLong("rating");
+            Matcher matcher = Pattern.compile("#\\s*(\\w+)").matcher(text.toLowerCase());
+            while (matcher.find()) {
+                if (hashMap.containsKey(matcher.group(1))) {
+                    double value = hashMap.get(matcher.group(1));
+                    value = value + rating;
+                    hashMap.put(matcher.group(1) , value);
+                } else {
+                    double value = rating;
+                    hashMap.put(matcher.group(1),value);
+                }
+            }
+
+        }
+        
+        for(String key : hashMap.keySet()){
+      	   double value = hashMap.get(key);
+      	   
+             String queryNew = "SELECT * FROM hashtags WHERE term LIKE '"+key+"' ";
+             Statement stmtNew = (Statement) connection.createStatement();
+             ResultSet rsNew = stmtNew.executeQuery(queryNew);
+             
+             if (rsNew.absolute(1)){
+          	   double num = rsNew.getDouble("value");
+          	   value = value/num;
+          	   hashMap2.put(key,value);
+             }
+         }
+         
+         sorted_map.putAll(hashMap2);
+         
+         count = 0;
+         
+         for(String key : sorted_map.keySet()){
+         	JSONObject json = new JSONObject();
+         	double value = hashMap2.get(key);
+         	json.put("key", key);
+         	json.put("value", value);
+         	mainjson.put(json);
+         	
+         	count++;
+         	if (count ==5)
+         		break;
+         }
+         
+		return mainjson;
+	}
+	
+	public JSONArray getHashTagInformation() throws ClassNotFoundException, SQLException, JSONException{
+        String query = "SELECT * FROM analysis_tweets_new "+init_query;
+        JSONArray mainjson = getInfo(query);
+        return mainjson;
+	}
+	
+	public JSONArray getTrendingInformation() throws ClassNotFoundException, SQLException, JSONException{
+		String query = "SELECT * FROM analysis_tweets_new ORDER BY seconds DESC LIMIT 1000";
+		JSONArray mainjson = getInfo(query);
 		return mainjson;
 	}
 	
@@ -177,7 +257,19 @@ public class SideBar {
 		JSONArray entity_information = getEntityInformation();
 		JSONArray trending_keyword = getKeywordInformation();
 		JSONArray trending_hashtag = getHashTagInformation();
-		return entity_information;
+		JSONArray trends = getTrendingInformation();
+		
+		System.out.print(trending_keyword);
+		System.out.print(trending_hashtag);
+		System.out.print(trends);
+		
+		JSONArray mainjson = new JSONArray();
+		mainjson.put(entity_information);
+		mainjson.put(trending_keyword);
+		mainjson.put(trending_hashtag);
+		mainjson.put(trends);
+		
+		return mainjson;
 	}
 
 	private Pair<Long, Long> getRank(long user_id) throws ClassNotFoundException, SQLException{
@@ -190,12 +282,10 @@ public class SideBar {
         long count = 0;
         long rank = 0;
 
-        String user = "MLStadium";
-
         System.out.println(count);
         while (rs.next()){
             count++;
-            if (rs.getString("mention_id").equals(user)){
+            if (rs.getString("mention_id").equals(entity)){
                 rank = count;
             }
         }
